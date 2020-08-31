b4e5986cff463e5b4c2e6e02445c21e1d534d0f8
Stefany <stefany.vasc.sa@gmail.com>
2020-08-31 15:54:13 -0300

    :construction: Create component IconSearch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import IconSearch from './components/IconSearch';
 import LogoMain from './components/LogoMain';
 
 function App() {
-  return <LogoMain />;
+  return (
+    <>
+      <LogoMain />
+      <IconSearch />
+    </>
+  );
 }
 
 export default App;
