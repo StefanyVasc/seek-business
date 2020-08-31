0f3b16d93c22889071f556a3b4c179e7e4bc2dcd
Stefany <stefany.vasc.sa@gmail.com>
2020-08-31 15:58:03 -0300

    :construction: Create component HeaderMain

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import HeaderMain from './components/HeaderMain';
 import IconSearch from './components/IconSearch';
 import LogoMain from './components/LogoMain';
 
 function App() {
   return (
-    <>
+    <HeaderMain>
       <LogoMain />
       <IconSearch />
-    </>
+    </HeaderMain>
   );
 }
 
---
+++ src/components/HeaderMain/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const HeaderMain = styled.header`
+  background-color: black;
+`;
+
+export default HeaderMain;
