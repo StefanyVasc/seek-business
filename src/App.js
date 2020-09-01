d9321d28664609474091c5d55b613022fabebbd9
Stefany <stefany.vasc.sa@gmail.com>
2020-08-31 23:30:56 -0300

    :construction: Create component IconFilter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import HeaderMain from './components/HeaderMain';
+import IconFilter from './components/IconFilter';
 import IconSearch from './components/IconSearch';
 import LogoMain from './components/LogoMain';
 
 function App() {
   return (
-    <HeaderMain>
-      <LogoMain />
-      <IconSearch />
-    </HeaderMain>
+    <>
+      <HeaderMain>
+        <LogoMain />
+        <IconSearch />
+      </HeaderMain>
+      <IconFilter />
+    </>
   );
 }
 
---
+++ src/components/IconFilter/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import iconFilter from '../../assets/img/icon-filter.svg';
+
+const IconFilter = styled.img.attrs({
+  src: iconFilter,
+  alt: 'Ícone que simboliza uma lista de itens',
+})`
+  background: #333;
+`;
+
+export default IconFilter;
