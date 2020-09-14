91fa151d0aa15eaf9612325a2691683ea0861bb2
Stefany <stefany.vasc.sa@gmail.com>
2020-09-14 14:05:56 -0300

    :construction: Add padding in HeaderMain

--- src/components/HeaderMain/index.js
+++ src/components/HeaderMain/index.js
@@ -5,12 +5,11 @@ import LogoMain from '../LogoMain';
 const HeaderMain = styled.header`
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  height: 66px;
+  padding: 16px 16px 28px;
 
   & > ${LogoMain}, & > ${IconSearch} {
     width: 24px;
-    margin: auto 16px;
+    /* margin: auto 16px; */
   }
 `;
 
